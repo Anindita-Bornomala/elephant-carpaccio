75a50ef28887c259084fbf2d44fbd6362609ed9d
Anindita Bornomala <aninborno@LAPTOP>
2024-01-18 21:28:36 -0500

    selling unitary goods in Ontario closes #1

--- mcmerch.py
+++ mcmerch.py
@@ -5,13 +5,17 @@ def main():
     tax = {"ON":0.13, "QC":0.14975, "MB":0.12}
 
     price = float(sys.argv[1])
-    
     quantity = int(sys.argv[2])
-
 
     pre_tax = price * quantity
 
+    tax = sys.argv[3]
+
+    if tax == "ON":
+        after_tax = pre_tax + (pre_tax*tax)
+
     print("The total cost is: $", pre_tax)
+    print("The total cost after tax is: $", after_tax)
 
 
 if __name__:
